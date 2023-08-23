afd27c4670b9351ecf9c0a0c709d98cb0c982b94
biel-igl <gabrielpires524@gmail.com>
2023-08-23 20:41:12 -0300

    faz exercico 05

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,1 +1,28 @@
 from pro_filer.actions.main_actions import show_disk_usage  # NOQA
+
+
+def test_show_disk_usage_anything(capsys):
+    context = {"all_files": []}
+
+    show_disk_usage(context)
+    captured = capsys.readouterr()
+    assert captured.out == "Total size: 0\n"
+
+
+def test_show_disk_usage(tmp_path, capsys):
+    file_path = tmp_path / "trybe.pdf"
+    file_path.write_text("Fake File")
+    file_path2 = tmp_path / "trybe2.pdf"
+    file_path2.write_text("Fake File de novo")
+
+    context = {"all_files": [str(file_path), str(file_path2)]}
+
+    show_disk_usage(context)
+    captured = capsys.readouterr()
+    assert (
+        captured.out
+        == "'/tmp/pytest-of-biel/pytest-...st_show_disk_usage0/trybe2.pdf':"
+        "        17 (65%)\n'/tmp/pytest-of-biel/pytest-"
+        "...est_show_disk_usage0/trybe.pdf':"
+        "        9 (34%)\nTotal size: 26\n"
+    )
